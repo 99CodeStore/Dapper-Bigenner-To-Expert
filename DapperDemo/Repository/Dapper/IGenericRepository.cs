9f9e694045a4b5e3b3181246732b358c578d00f0
Bheemsen Singh <bheem_stn@hotmail.com>
2021-12-13 12:07:19 +0530

    Generic in Dapper

---
+++ DapperDemo/Repository/Dapper/IGenericRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace DapperDemo.Repository.Dapper
+{
+    public interface IGenericRepository
+    {
+        string ConnectionString { get; set; }
+
+        void Execute(string name, CommandType commandType = CommandType.StoredProcedure);
+        void Execute(string name, object param, CommandType commandType = CommandType.StoredProcedure);
+        List<T> List<T>(string name, CommandType commandType = CommandType.StoredProcedure);
+        List<T> List<T>(string name, int id, CommandType commandType = CommandType.StoredProcedure);
+        List<T> List<T>(string name, object param, CommandType commandType = CommandType.StoredProcedure);
+        Tuple<IEnumerable<T1>, IEnumerable<T2>, IEnumerable<T3>> List<T1, T2, T3>(string name, object param, CommandType commandType = CommandType.StoredProcedure);
+        Tuple<IEnumerable<T1>, IEnumerable<T2>> List<T1, T2>(string name, object param, CommandType commandType = CommandType.StoredProcedure);
+        void QueryExecute(string name, CommandType commandType = CommandType.Text);
+        void QueryExecute(string name, object param, CommandType commandType = CommandType.Text);
+        T Single<T>(string name, int id, CommandType commandType = CommandType.StoredProcedure);
+        T Single<T>(string name, object param, CommandType commandType = CommandType.StoredProcedure);
+    }
+}
